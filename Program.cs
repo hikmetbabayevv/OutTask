6afe37a3c95e927598ebf99de7a0f48492e2e929
hikmetbabayev <hikmetbabayev319@gmail.com>
2023-05-08 13:46:02 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace OutTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Modify(out person);
+            Console.WriteLine(person.Name);
+           
+        }
+        static void Modify(out Person person)
+        {
+            person = new Person();
+            person.Age += 1;
+            person.Name = "Hikmet";
+        }
+        class Person
+        {
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+            public Person()
+            {
+                
+            }
+            public string Name { get; set; }    
+            public int Age { get; set; }
+        }
+    }
+}
